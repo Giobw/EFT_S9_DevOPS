6c5185060f1906743b78057d948f2eba9334a4c9
Giobw <gmebencini@gmail.com>
2026-07-19 15:08:26 -0400

    Update UsuarioModel.java

--- src/main/java/com/usuarios/UsuariosRest/models/UsuarioModel.java
+++ src/main/java/com/usuarios/UsuariosRest/models/UsuarioModel.java
@@ -1,52 +1,38 @@
 package com.usuarios.UsuariosRest.models;
 
 import jakarta.persistence.*;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Entity
 @Table(name = "users")
 public class UsuarioModel {
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @Column
+    @Column(name = "firstName")
+    @JsonProperty("firstName")
     private String firstName;
 
-    @Column
+    @Column(name = "lastName")
+    @JsonProperty("lastName")
     private String lastName;
 
-    @Column
+    @Column(name = "email")
+    @JsonProperty("email")
     private String email;
+
+    // Getters y Setters
+    public long getId() { return id; }
+    public void setId(long id) { this.id = id; }
+
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
 }
